02334d7ff5778517ccf7cb13004dd5b92300ba67
Adonis Mendoza <adonis.mendoza@outlook.com>
2018-06-21 12:03:41 -0400

    [Add] Results for teams (wins, losses)

--- src/component/pages/results.js
+++ src/component/pages/results.js
@@ -1,31 +1,38 @@
 import React, { Component } from 'react';
-
+import './results.css'
 class Results extends Component{
     constructor() {  
         super();  
         this.state = {  
             ResulttData: []  
-        } 
-        this.searchResults = this.searchResults.bind(this); 
+        }
     }
-    searchResults(){
+    componentDidMount(){
         fetch('http://worldcup.sfg.io/teams/results')
         .then((resp) => resp.json())
         .then((data)=>{
-            console.log(data);
-            this.setState={
-                ResulttData: data
-            }
-        })
+            let ResulttData = data.map((dt,i)=>{
+                return(
+                    <div className="col-sm-3 mb-3 ml-3">
+                        <div className="card" key={i}>
+                        <div class="card-header">{dt.country}</div>
+                            <ul className="list-group list-group-flush">
+                                <li className="list-group-item">Wins: {dt.wins}</li>
+                                <li className="list-group-item"> losses: {dt.losses}</li>
+                            </ul>
+                        </div>
+                    </div>
+                )
+            });
+
+           this.setState({ResulttData:ResulttData});
+        });
     }
     render(){
-        return(
-            <div>
-                    <p>
-                        {this.searchResults()}
-                        {this.ResulttData && this.ResulttData.map((item,key)=> <li key={key}>{item.country}</li>)}
-                    </p>
-            </div>
+        return (
+            <div className="container">
+                <div className="row"> { this.state.ResulttData}</div>
+            </div>    
         )
     }
 }
